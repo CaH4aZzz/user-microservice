ebd074c88e6629b106498f0463c228fa5fe7b0a5
Aleksandr Prykhodko <CaH4aZzz@ua.fm>
2020-01-16 12:18:03 +0200

    test commit

--- src/main/java/com/test/usermicroservice/UserMicroserviceApplication.java
+++ src/main/java/com/test/usermicroservice/UserMicroserviceApplication.java
@@ -16,5 +16,4 @@ public class UserMicroserviceApplication {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-
 }
--- src/main/java/com/test/usermicroservice/repository/UserRepository.java
+++ src/main/java/com/test/usermicroservice/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.test.usermicroservice.repository;
 
 import com.test.usermicroservice.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@RequestMapping
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     Optional<User> getUserByUsername(String username);
